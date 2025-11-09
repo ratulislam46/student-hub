96998e57a4ac2fe2d0e61920c8e3136dd2b70244
ratulislam46 <arfanratul46@gmail.com>
2025-11-09 22:34:20 +0600

    Added profile page and show user data

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -7,7 +7,7 @@ const Input = ({ type, name, placeholder, label }) => {
                 type={type}
                 name={name}
                 autoComplete='current info'
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-400 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder={placeholder}
                 required />
             <label
--- src/Page/Profile/Profile.jsx
+++ src/Page/Profile/Profile.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { use } from 'react';
+import { AuthContext } from '../../Context/AuthProvider';
 
 const Profile = () => {
+    const {user} = use(AuthContext);
     return (
         <div>
             <h1>Profile</h1>
+            <img src={user?.photoURL} alt="USER" />
+            <p>Name: {user?.displayName || 'User'}</p>
+            <p>Email: {user?.email}</p>
+            <p>Created At: {user?.metadata?.createdAt}</p>
+            <p>Last Login: {user?.metadata?.lastLoginAt}</p>
+            <p>Phone Number: {user?.phoneNumber || "N/A"}</p>
+            <p>UID: {user?.uid}</p>
         </div>
     );
 };
